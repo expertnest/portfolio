7579dfb26006cf6dd1b737d032d16d0264d706c6
Miguel <miguel@Miguels-Laptop.local>
2024-11-14 15:25:44 -0500

    updated portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
  
 import PageWrapper from "@/components/wrapper/page-wrapper";
-import { ShootingStarsAndStarsBackgroundDemo } from "@/components/ui/ShootingStarsAndStarsBackgroundDemo";
-import { TimelineDemo } from "@/components/ui/TimelineDemo";
-import MarketingCards from "@/components/ui/marketing-cards";
-import About from "@/components/homepage/About";
+ 
 import { AppleCardsCarouselDemo } from "@/components/ui/AppleCardsCarouselDemo";
-import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
-import { HyperTextDemo } from '@/components/ui/HyperTextDemo'
-
+ 
 
 export default function Home() {
   return (
